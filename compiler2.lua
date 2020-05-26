fcbe96b91e186483ced08052989a2bbf4df6c629
Ryan Ward <rayaman99@gmail.com>
2020-05-25 23:01:06 -0400

    Designing structure of the language

--- compiler2.lua
+++ compiler2.lua
@@ -1,6 +1,18 @@
 file = io.open("test.dms","rb")
 content = file:read("*a")
 line_num = 0
+CMD = {}
+CMD.__index = CMD
+function CMD:new(ln,cmd,args)
+    local c = {}
+    setmetatable(c,self)
+    c.line_num = ln
+    c.command = cmd
+    c.args = args
+end
+function CMD:process()
+    
+end
 function string:trim()
     return (self:gsub("^%s*(.-)%s*$", "%1"))
 end
@@ -8,12 +20,16 @@ local choice
 for line in content:gmatch("(.-)\n") do
     line_num = line_num + 1
     line = line:trim()
+    line = line:gsub("")
+    if line=="" then goto continue end
     ::back::
     if line:match("^%[[_:,%w%(%)]+%]") then
         print(line_num,"BLOCK_START",line)
     elseif line:match("choice%s+\".+\"%s*:") then
         print(line_num,"CHOICE_BLOCK",line)
         choice = true
+    elseif line:match("::([_:,%w%(%)]+)::") then
+        print(line_num,"LABEL_BLOCK",line)
     elseif choice then
         choice = false
         if line:match("\".*\"%s*[_:,%w%(%)]+%(.*%)") then
@@ -25,4 +41,5 @@ for line in content:gmatch("(.-)\n") do
     else
         print(line_num,line)
     end
+    ::continue::
 end
